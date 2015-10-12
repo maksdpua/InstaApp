02b27a2aaaa9cdb2afcebcbdfe66c0dc9a07933e
maksdpua <maksdpua@gmail.com>
2015-10-12 22:37:33 +0300

    Initial Commit

---
+++ InstaApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  InstaApp
+//
+//  Created by Maks on 10/12/15.
+//  Copyright © 2015 Maks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ InstaApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstaApp
+//
+//  Created by Maks on 10/12/15.
+//  Copyright © 2015 Maks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
